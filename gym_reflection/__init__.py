16dab569b4cf12c04722a3b2727a523ea566ed30
BLDG18 <kyle.i.evered@lmco.com>
2018-07-31 08:31:29 -0400

    fixed entry point line in gym_reflection

--- gym_reflection/__init__.py
+++ gym_reflection/__init__.py
@@ -6,6 +6,6 @@ logger = logging.getLogger(__name__)
 
 register(
     id='reflection-v0',
-    entry_point='gym_reflection.envs.ReflectionEnv',
+    entry_point='gym_reflection.envs:ReflectionEnv',
     reward_threshold=1.0,
 )
